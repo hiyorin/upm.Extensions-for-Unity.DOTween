610451cba5cb0a1779f7950456eecc6f96bb57a3
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2019-02-02 03:26:06 +0900

    [add] move scripts

---
+++ Scripts/AudioSourceExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UniRx;
+using DG.Tweening;
+
+namespace UnityExtensions
+{
+    public static class AudioSourceExtensions
+    {
+        public static void FadeIn(this AudioSource self, float volume, float duration)
+        {
+            if (volume < 0.0f) volume = 0.0f;
+            else if (volume > 1.0f) volume = 1.0f;
+            DOTween.To(() => self.volume, x => self.volume = x, volume, duration).SetTarget(self);
+        }
+    
+        public static IEnumerator FadeInAsCoroutine(this AudioSource self, float volume, float duration)
+        {
+            self.FadeIn(volume, duration);
+            yield return new WaitForSeconds(duration);
+        }
+    
+        public static void FadeOut(this AudioSource self, float duration)
+        {
+            DOTween.To(() => self.volume, x => self.volume = x, 0.0f, duration).SetTarget(self);
+        }
+
+        public static IEnumerator FadeOutAsCoroutine(this AudioSource self, float duration)
+        {
+            self.FadeOut(duration);
+            yield return new WaitForSeconds(duration);
+        }
+    
+        public static bool PlayWithFadeIn(this AudioSource self, AudioClip clip, float volume, float duration)
+        {
+            if (clip == null || volume <= 0.0f)
+                return false;
+
+            self.clip = clip;
+            self.volume = 0.0f;
+            self.Play();
+            self.FadeIn(volume, duration);
+            return true;
+        }
+        
+        public static IObservable<Unit> PlayWithFadeInAsObservable(this AudioSource self, AudioClip clip, float volume, float duration)
+        {
+            if (self.PlayWithFadeIn(clip, volume, duration))
+                return Observable.Timer(TimeSpan.FromSeconds(clip.length)).AsUnitObservable();
+            else
+                return Observable.Throw<Unit>(new ArgumentException());
+        }
+    }
+}
---
+++ Scripts/DOTweenExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UnityEngine;
+using DG.Tweening;
+using UniRx;
+using UniRx.Async;
+
+namespace UnityExtensions
+{
+    public static class DOTweenExtensions
+    {
+        public static IObservable<Unit> OnFinishAsObservable(this Tween self)
+        {
+            return new WaitUntil(() => !self.IsPlaying()).ToObservable();
+        }
+
+        public static IObservable<Unit> OnCompleteAsObservable(this Tween self)
+        {
+            return Observable.FromEvent<TweenCallback>(
+                x => x.Invoke,
+                x => self.onComplete += x,
+                x => self.onComplete -= x);
+        }
+
+        #if UNITY_2018_3_OR_NEWER
+        public static async UniTask OnCompleteAsUniTask(this Tween self)
+        {
+            await Observable.FromEvent<TweenCallback>(
+                    x => x.Invoke,
+                    x => self.onComplete += x,
+                    x => self.onComplete -= x)
+                .First();
+        }
+        #endif
+        
+        public static IObservable<Unit> OnPlayAsObservable(this Tween self)
+        {
+            return Observable.FromEvent<TweenCallback>(
+                x => x.Invoke,
+                x => self.onPlay += x,
+                x => self.onPlay -= x);
+        }
+
+        public static IObservable<Unit> OnPauseAsObservalbe(this Tween self)
+        {
+            return Observable.FromEvent<TweenCallback>(
+                x => x.Invoke,
+                x => self.onPause += x,
+                x => self.onPause -= x);
+        }
+
+        public static IObservable<Unit> OnUpdateAsObservalbe(this Tween self)
+        {
+            return Observable.FromEvent<TweenCallback>(
+                x => x.Invoke,
+                x => self.onUpdate += x,
+                x => self.onUpdate -= x);
+        }
+
+        public static IObservable<Unit> OnRewindAsObservalbe(this Tween self)
+        {
+            return Observable.FromEvent<TweenCallback>(
+                x => x.Invoke,
+                x => self.onRewind += x,
+                x => self.onRewind -= x);
+        }
+
+        public static IObservable<Unit> OnStepCompleteAsObservalbe(this Tween self)
+        {
+            return Observable.FromEvent<TweenCallback>(
+                x => x.Invoke,
+                x => self.onStepComplete += x,
+                x => self.onStepComplete -= x);
+        }
+
+        public static IObservable<int> OnWaypointChangeAsObservalbe(this Tween self)
+        {
+            return Observable.FromEvent<TweenCallback<int>, int>(
+                x => y => x.Invoke(y),
+                x => self.onWaypointChange += x,
+                x => self.onWaypointChange -= x);
+        }
+
+        public static IObservable<Unit> OnKillAsObservalbe(this Tween self)
+        {
+            return Observable.FromEvent<TweenCallback>(
+                x => x.Invoke,
+                x => self.onKill += x,
+                x => self.onKill -= x);
+        }
+    }
+}
